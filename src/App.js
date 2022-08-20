6187950badd3cc1daa34432c6364092962351d2c
asami0119 <assa.0119.0425.0905@gmail.com>
2022-08-20 19:24:03 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <header className="App-header"></header>
+      <div className="container">
+        <button className="btn btn-primary">hello</button>
+        <p>
+          This project was coded by
+          <a
+            href="https://www.linkedin.com/in/asami-uehara-704999223/"
+            target="_blank"
+          >
+            {" "}
+            Asami Uehara
+          </a>{" "}
+          and is open-sourced on
+          <a
+            href="https://github.com/asami0119/react-week5-weather"
+            target="_blank"
+          >
+            {" "}
+            GitHub
+          </a>
+        </p>
+        <header className="App-header"></header>
+      </div>
     </div>
   );
 }
